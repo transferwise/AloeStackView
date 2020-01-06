86bf308143d52645e110653c276ba261f3c8acdd
Nick Banks <nick.banks@synchromation.com>
2020-01-06 17:17:12 +0000

    Add Package.swift
    # Conflicts:
    #	Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "AloeStackView",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(
+            name: "AloeStackView",
+            targets: ["AloeStackView"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "AloeStackView",
+            dependencies: [],
+            path: "Sources"),
+    ]
+)
